14cb67fd71438865c9661fd12ef515b80a4fe560
François Poirotte <clicky@erebot.net>
2018-08-15 23:40:20 +0200

    Add proper support for the portlist type
    The value is now checked for invalid values (things which are not ports
    nor ranges, or invalid ports). For the purpose of this type, "0" is taken
    as an invalid port number.

--- src/Types/PortListType.php
+++ src/Types/PortListType.php
@@ -5,4 +5,25 @@ namespace fpoirotte\IDMEF\Types;
 class PortListType extends AbstractType
 {
     const XML_TYPE = 'portlist';
+
+    public function __construct($value)
+    {
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException($value);
+        }
+
+        foreach (explode(',', $value) as $ports) {
+            foreach (explode('-', $ports, 2) as $port) {
+                if ($port === '' || strspn($port, '1234567890') !== strlen($port)) {
+                    throw new \InvalidArgumentException($value);
+                }
+                $port = (int) $port;
+                if ($port < 1 || $port > 65535) {
+                    throw new \InvalidArgumentException($value);
+                }
+            }
+        }
+
+        $this->_value = $value;
+    }
 }
